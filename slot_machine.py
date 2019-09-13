188579bafb62323ed1c9f4b154c6948cff346b7b
Bob Whitmer <rjwhitmercc@gmail.com>
2019-09-13 17:21:27 -0600

    updated bet structure

--- slot_machine.py
+++ slot_machine.py
@@ -6,14 +6,15 @@ and pull the lever! Match 3 symbols and win!
 $$$=4x your bet
 ***=8x your bet
 777=Win the jackpot!
-ope
+
 """
 import random
+import time
 
 
 symbols=('@', '$', '*', '7', '%', '#', '^')
 
-def pullLever(bet):
+def pullLever(x):
     prize=[]
     for x in range(0, 3):
         prize.append(random.choice(symbols))
@@ -23,23 +24,39 @@ jackpot=0
 balance=0
 balance=float(input("Deposit money to play the slot machine! "))
 while balance>0:
+    x=0
     jackpot+=10
     print(balance,"  This is your balance")
-    bet=float(input("Place your bet!"))
-    prize=''.join(pullLever(bet))
+    print("Jackpot is at ", jackpot)
+    bet=float(input("Place your bet! "))
+    print()
+    if bet>balance:
+        i=input("Insufficient funds. Use remaining balance for bet? Y/N :")
+        if i=="y":
+            print("Your bet will be ", balance)
+            bet=balance
+        else:
+            continue
+    prize=''.join(pullLever(x))
+    time.sleep(.5)
     print(prize)
     if prize=='@@@':
         print("Hey! You won ",(bet*2))
+        print('------------------------')
         balance=balance+(bet*2)
     elif prize=='$$$':
         print("Whoa! You won ",(bet*4))
+        print('------------------------')
         balance=balance+(bet*4)
     elif prize=='***':
         print("Yowza! You won ",(bet*8))
+        print('------------------------')
         balance=balance+(bet*8)
     elif prize=='777':
         print("DING DING DING DING DING!!!! YOU WON THE JACKPOT OF ",jackpot)
+        print('------------------------')
         balance=balance+jackpot
     else:
         balance=balance-bet
         print("Better luck next time!")
+        print('------------------------')
